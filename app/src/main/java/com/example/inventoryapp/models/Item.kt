6ec61c4e60b341b757a9f68f105aa3c202b948a3
Hannah May Tabunan <hannah.tabunan@inspirex.io>
2023-03-03 18:31:09 +0800

    Create the item entity

--- app/src/main/java/com/example/inventoryapp/models/Item.kt
+++ app/src/main/java/com/example/inventoryapp/models/Item.kt
@@ -1,13 +1,25 @@
 package com.example.inventoryapp.models
 
-import java.time.LocalDate
-import java.time.LocalDateTime
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
 import java.util.*
 
+@Entity(tableName = "items_tbl")
 data class Item(
+    @PrimaryKey
     val id: UUID = UUID.randomUUID(),
+
+    @ColumnInfo(name = "item_name")
     val name: String,
+
+    @ColumnInfo(name = "item_price")
     val price: Double,
+
+    @ColumnInfo(name = "item_stock")
     val stock: Int,
-    val entryDate: LocalDateTime = LocalDateTime.now()
+
+    @ColumnInfo(name = "item_entry_date")
+    val entryDate: Date = Date.from(Instant.now())
 )
