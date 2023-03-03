b1b3ee7bbacfcfe4f92cea6111563dbeffc71924
Hannah May Tabunan <hannah.tabunan@inspirex.io>
2023-03-03 19:55:32 +0800

    Update the ItemViewModel class

--- app/src/main/java/com/example/inventoryapp/screens/ItemViewModel.kt
+++ app/src/main/java/com/example/inventoryapp/screens/ItemViewModel.kt
@@ -1,26 +1,44 @@
 package com.example.inventoryapp.screens
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.inventoryapp.data.ItemsDatasource
 import com.example.inventoryapp.models.Item
+import com.example.inventoryapp.repository.InventoryRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ItemViewModel : ViewModel() {
-    var itemList = mutableStateListOf<Item>()
+@HiltViewModel
+class ItemViewModel @Inject constructor(private val repository: InventoryRepository) : ViewModel() {
+//    var itemList = mutableStateListOf<Item>()
+
+    private val _itemList = MutableStateFlow<List<Item>>(emptyList())
+    val itemList = _itemList.asStateFlow()
 
     init {
-        itemList.addAll(ItemsDatasource().loadItems())
+//        itemList.addAll(ItemsDatasource().loadItems())
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getAllItems().distinctUntilChanged().collect { listOfItems ->
+                if (listOfItems.isNullOrEmpty()) {
+                    Log.d("Empty", ": Empty list")
+                } else {
+                    _itemList.value = listOfItems
+                }
+            }
+        }
     }
 
-    fun addItem(item: Item) {
-        itemList.add(item)
-    }
+    suspend fun addItem(item: Item) = viewModelScope.launch { repository.addItem(item) }
 
-    fun removeItem(item: Item) {
-        itemList.remove(item)
-    }
+    suspend fun updateItem(item: Item) = viewModelScope.launch { repository.updateItem(item) }
 
-    fun getAllItems(): List<Item> {
-        return itemList
-    }
+    suspend fun removeItem(item: Item) = viewModelScope.launch { repository.deleteItem(item) }
 }
