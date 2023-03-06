f81215ea76e5bb39aa6bbc93cc78131235f2c74f
Hannah May Tabunan <hannah.tabunan@inspirex.io>
2023-03-06 16:30:40 +0800

    Adding TypeConverters and getting the app work

--- app/src/main/java/com/example/inventoryapp/data/InventoryDatabase.kt
+++ app/src/main/java/com/example/inventoryapp/data/InventoryDatabase.kt
@@ -2,9 +2,14 @@ package com.example.inventoryapp.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import com.example.inventoryapp.models.Item
+import com.example.inventoryapp.util.DateConverter
+import com.example.inventoryapp.util.UUIDConverter
 
 @Database(entities = [Item::class], version = 1, exportSchema = false)
+@TypeConverters(DateConverter::class, UUIDConverter::class)
 abstract class InventoryDatabase: RoomDatabase() {
     abstract fun inventoryDao(): InventoryDatabaseDao
 }
