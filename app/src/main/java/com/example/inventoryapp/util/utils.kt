635d1f5e341ab45812b1043d7cac309da79bc7d5
Hannah May Tabunan <hannah.tabunan@inspirex.io>
2023-03-07 13:00:55 +0800

    Add utils package

---
+++ app/src/main/java/com/example/inventoryapp/util/utils.kt
@@ -0,0 +1,11 @@
+package com.example.inventoryapp.util
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun formatDate(time: Long): String {
+    val date = Date(time)
+    val format = SimpleDateFormat("EEE, d MM hh:mm aaa", Locale.getDefault())
+
+    return format.format(date)
+}
