eb062fe600401e55af6daf4e713728ddbc4a1efd
Hannah May Tabunan <hannah.tabunan@inspirex.io>
2023-03-07 21:10:13 +0800

    Remove unnecessary imports in Homescreen

--- app/src/main/java/com/example/inventoryapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/inventoryapp/screens/HomeScreen.kt
@@ -1,30 +1,23 @@
 package com.example.inventoryapp.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.rounded.List
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.inventoryapp.components.DrawerContent
 import com.example.inventoryapp.components.ItemRow
-import com.example.inventoryapp.components.bottomBorder
 import com.example.inventoryapp.data.ItemsDatasource
 import com.example.inventoryapp.models.Item
-import com.example.inventoryapp.navigation.Itemscreens
 import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
